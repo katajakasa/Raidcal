5bc87b49215cb7c26e76612ae6996dc7fd0115a0
Tuomas Virtanen <katajakasa@gmail.com>
2015-11-09 22:56:54 +0200

    Properly show up event details

--- raidcal/maincal/views.py
+++ raidcal/maincal/views.py
@@ -6,7 +6,7 @@ from forms import LoginForm, RegisterForm
 from models import Event
 from custom.utils import ts_to_dt
 
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
@@ -74,7 +74,12 @@ def logged_out(request):
 
 @login_required()
 def event(request, event_id):
-    pass
+    cal_event = get_object_or_404(Event, pk=event_id)
+
+    return render_to_response('maincal/event.html', {
+        'page': 'event',
+        'event': cal_event,
+    }, context_instance=RequestContext(request))
 
 
 @login_required()
